c43c63e93d3687f48d565871f7edd1382302444a
marciofaria-git <devmarciofaria@gmai.com>
2022-08-18 20:03:18 -0300

    add flags ts-node-dev, created first route, disable strict mode

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const portApplication = 3333
 
 const app = express();
 
+app.get("/",(request, response)=>{
+  return response.json({message:"Hellow World!"})
+})
+
 app.listen(portApplication, ()=> console.log("Server is running!"));
