121756b23aab5a40daf85b3c284ac622e3461511
marciofaria-git <devmarciofaria@gmai.com>
2022-08-18 20:22:11 -0300

    create route /todo for post todos, and call use express.json

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,15 @@ const portApplication = 3333
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/",(request, response)=>{
   return response.json({message:"Hellow World!"})
+})
+
+app.post("/todo",(request, response)=>{
+  const {item} = request.body;
+  return response.json({item});
 })
 
 app.listen(portApplication, ()=> console.log("Server is running!"));
