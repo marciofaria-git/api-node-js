aa068fad5c75faf957122f835ab9f36a0a5e486f
marciofaria-git <devmarciofaria@gmai.com>
2022-08-22 20:13:33 -0300

    using object assing to post the information

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,11 +9,17 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = { name, description, created_at: new Date(), };
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({categories});
 })
 
 export { categoriesRoutes };
