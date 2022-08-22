baa4780bd3ea79c96284ddb9d2c3a6244b438fb7
marciofaria-git <devmarciofaria@gmai.com>
2022-08-22 19:57:42 -0300

    add uuid for categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express'
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response)=>{
-  const { name, description} = request.body;
+categoriesRoutes.post("/categories", (request, response) => {
+  const { name, description } = request.body;
 
-  categories.push({name, description});
+  const category = { name, description, id: uuidV4() };
+
+  categories.push(category);
 
   return response.status(201).send();
 })
 
-export {categoriesRoutes};
+export { categoriesRoutes };
