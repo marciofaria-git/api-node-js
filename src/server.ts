b70f804e9bf259d5a8f5980e45543ccd0e34e887
marciofaria-git <devmarciofaria@gmai.com>
2022-09-01 21:23:09 -0300

    add route for create a new specifications

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const portApplication = 3333;
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/categories",categoriesRoutes);
+app.use("/specifications", specificationsRoutes)
 
 
 app.listen(portApplication, ()=> console.log("Server is running!"));
